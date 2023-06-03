3dfef0814dde571646e30930f21106a5e8134b4d
elbopero <amanboparai@live.no>
2023-06-03 18:29:11 +0200

    created react app inside frontend folder

---
+++ frontend/src/components/Navbar.js
@@ -0,0 +1,13 @@
+import { Link } from 'react-router-dom'
+
+const Navbar = () => {
+    return (
+        <header>
+            <div className="container">
+                <h1>Workout Buddy</h1>
+            </div>
+        </header>
+    )
+}
+
+export default Navbar

6aae5b5fd4b3b456e0f1eecfa9e2c98430dede0a
elbopero <amanboparai@live.no>
2023-06-03 21:05:47 +0200

    :sparkles: Added workout form that is connected to the db

--- frontend/src/pages/Home.js
+++ frontend/src/pages/Home.js
@@ -2,6 +2,8 @@ import { useEffect, useState} from 'react'
 
 // components
 import WorkoutDetails from '../components/WorkoutDetails'
+import WorkoutForm from '../components/WorkoutForm'
+
 
 const Home = () => {
     const [workouts, setWorkouts] = useState(null)
@@ -26,6 +28,7 @@ const Home = () => {
                     <WorkoutDetails key={workout._id} workout={workout}/>
                 ))}
             </div>
+            <WorkoutForm/>
         </div>
     )
 }

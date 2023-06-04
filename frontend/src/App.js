07b3a80da9a14e8996ec058d5336e1ba4fa21078
elbopero <amanboparai@live.no>
2023-06-05 00:11:55 +0200

    :memo:added login and signup forms, included button for both in navbar

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -2,6 +2,8 @@ import { BrowserRouter, Routes, Route} from 'react-router-dom'
 
 // pages & components
 import Home from './pages/Home'
+import Signup from './pages/Signup'
+import Login from './pages/Login'
 import Navbar from './components/Navbar'
 
 function App() {
@@ -14,6 +16,16 @@ function App() {
           <Route
           path="/"
           element={<Home />}>
+
+          </Route>
+          <Route
+          path="/login"
+          element={<Login />}>
+
+          </Route>
+          <Route
+          path="/signup"
+          element={<Signup />}>
 
           </Route>
         </Routes>

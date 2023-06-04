a7d42ee735ec6c0f323464c8e182f5f05099aee6
elbopero <amanboparai@live.no>
2023-06-04 10:00:56 +0200

    :memo:added user routes, controllfunction and model

--- backend/server.js
+++ backend/server.js
@@ -3,6 +3,8 @@ require('dotenv').config()
 const express = require('express')
 const mongoose = require('mongoose')
 const workoutRoutes = require('./routes/workouts')
+const userRoutes = require('./routes/user')
+
 
 
 //express app
@@ -23,8 +25,11 @@ mongoose.connect(process.env.MONGO_URI)
     .catch((error) => {
         console.log(error)
     })
+    
 // routes
 app.use('/api/workouts' ,workoutRoutes)
+app.use('/api/user/' , userRoutes)
+
 
 
 // listen for requests
